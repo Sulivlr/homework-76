31f18a4956ab61eb18cb1addbb5839cbba13fe72
Sultan Mukhtarov <suli419956@gmail.com>
2024-08-12 13:50:10 +0600

    created fileDb with data saving. Added if check in case if either author or message will be not fulfilled should return status 404 err otherwise returning msgdata and adding to fileDb.

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from 'cors';
 import messagesRouter from "./routes/messages";
+import fileDb from './fileDb';
+
 
 const app = express();
 const port = 8000;
@@ -8,7 +10,16 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use('/messages', messagesRouter);
-app.listen(port, () => {
+
+
+const run = async () => {
+  await fileDb.init();
+  app.listen(port, () => {
     console.log('Server running on port', port);
-});
+  });
+};
+
+void run();
+
+
 
--- backend/routes/messages.ts
+++ backend/routes/messages.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import {MessageMutation} from '../types';
+import fileDb from '../fileDb';
 
 const messagesRouter = express.Router();
 
@@ -6,7 +8,20 @@ messagesRouter.get("/", (req, res) => {
    return res.send('SomeThing');
 });
 
-messagesRouter.post("/", (req, res) => {
+messagesRouter.post("/",  async (req, res) => {
+    if (!req.body.message || !req.body.author) {
+      return res.status(400).send({error: "Author and message must be present in the request"});
+    }
+
+    const messageData: MessageMutation = {
+      message: req.body.message,
+      author: req.body.author,
+    };
+
+    const message = await fileDb.addMessages(messageData);
+
+    res.send(message);
+
     return res.send('SomeThing post');
 })
 
